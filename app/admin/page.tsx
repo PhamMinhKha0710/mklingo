517ad4396f20a3b98ee7006ce83c260036187691
PhamMinhKha0710 <phamminhkha19091710@gmail.com>
2026-03-10 15:08:36 +0700

    fix: use dynamic import with ssr:false for admin page to fix prerender error

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import App from "./app";
+const App = dynamic(() => import("./app"), { ssr: false });
 
 const AdminPage = () => {
     return (
